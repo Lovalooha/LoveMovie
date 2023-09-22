4ab52411648edeb0e125985b0252f77f09316ee2
erwinkang <erwinkangnata@gmail.com>
2023-09-22 22:49:33 +0700

    first commit

---
+++ js/banner.js
@@ -0,0 +1,14 @@
+let movies = [
+    {
+        image: 'img/Banner/banner1.jpg'
+    },
+    {
+        image: 'img/Banner/banner2.webp'
+    },
+    {
+        image: 'img/Banner/banner2.jpg'
+    },
+    {
+        image: 'img/Banner/banner4.jpg'
+    }
+]
